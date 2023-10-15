3d677c4199c5c766c2793b502f925108ea3ecea6
Кирилл Муравьев <ovaqvado@mail.ru>
2023-10-15 23:32:47 +0300

    hello js

---
+++ main.js
@@ -0,0 +1,167 @@
+     //                  1 ЗАДАНИЕ
+
+
+
+// let age = prompt("Ваш возраст:");
+
+// if (age >= 0 && age < 12) {
+//     alert("Вы ребенок");
+// } else if (age >= 12 && age <18){
+//     alert("Вы подросток")
+// } else if (age >=18 && age < 60) {
+//     alert("Вы Взрослый")
+// } else {
+//     alert("Вы пенсионер")}
+
+
+                               // ЗАДАНИЕ 2
+
+
+
+// let number = prompt("Ваш ноиер 0-9");
+
+// let symbol = '';
+
+// switch (number){
+// case"0":
+//     symbol = ")"
+//     break;
+// case"1":
+//     symbol = "!"
+//     break;
+// case"2":
+//     symbol = "@"
+//     break;
+// case"3":
+//     symbol = "#"
+//     break;
+// case"4":
+//     symbol = "$"
+//     break;
+// case"5":
+//     symbol = "%"
+//     break;
+// case"6":
+//     symbol = "^"
+//     break;
+// case"7":
+//     symbol = "&"
+//     break;
+// case"8":
+//     symbol = "*"
+//     break;
+// case"9":
+//     symbol = "("
+//     break;
+// }
+
+// alert(symbol);
+
+
+                                    // ЗАДАНИЕ 3
+
+
+//  let numberthree = prompt("Введите число состоящие из трех ццифр");
+//  let spl = numberthree.split("");
+// let calc = 0;
+// if (spl[0] == spl[1]) {
+//     calc++;
+// } if (spl[0] == spl[2]) {
+//     calc++;
+// } if (spl[0] == spl [3]) {
+//     calc++;
+// } if (spl[0] == spl [4]) {
+//     calc++;
+// } if (spl[0] == spl [5]) {
+//     calc++;
+// } if (spl[0] == spl [6]) {
+//     calc++;
+// } if (spl[0] == spl [7]) {
+//     calc++;
+// } if (calc == 1) {
+//     calc++;
+// } 
+// alert(`Мы обнаружили что вы ввели ${calc} одинаковых цифр`)
+
+
+
+                            //  ЗАДАНИЕ 4
+
+// let years = Math.trunc(prompt("Високосный год или нет: введите год "));
+//   alert(!(years % 400) || (!(years % 4) && years % 100)? "Этот год високосный!": "Этот год не високосный");
+
+
+                        //  ЗАДАНИЕ 5
+                        // НЕ ПОНЯЛ
+
+                        // ЗАДАНИЕ 6
+                        // НЕ ПОНЯЛ
+
+
+                        // ЗАДАНИЕ 7
+// let discount = Math.abs(+prompt("Введите сумму вашего товара "));
+// let Sale = Number(0);
+// if (discount >= 200 && discount <= 300) {
+//   Sale = 3;
+//   discount = discount - (discount * Sale) / 100;
+// }
+// if (discount > 300 && discount <= 500) {
+//   Sale = 5;
+//   discount = discount - (discount * Sale) / 100;
+// }
+// if (discount > 500) {
+//   Sale = 5;
+//   discount = discount - (discount * Sale) / 100;
+// }
+
+// alert(`Товаров на сумму ${discount / (1 - Sale / 100)} ваша скидка ${Sale}% - ${((discount / (1 - Sale / 100)) * Sale) / 100} К оплате будет ${discount} рублей.`);
+
+                        // ЗАДАНИЕ 8
+
+//    let bordradius = +prompt("radius cyrcle ");
+//    let p = +prompt("perimetr square ");
+//    if (bordradius / 2 / Math.PI > p / 4 / 2) {
+//    alert("Окружность к сожалению не сможет вписаться в квадрат (Попробуйте изменить параметры или проверьте еще раз данные которые вы написали)");
+//    } else {
+//    alert("Окружность сможет вписаться в квадрат)");
+//    }
+
+
+
+
+                        // ЗАДАНИЕ 9     НЕ СМОГ ДО КОНЦА ПОНЯТЬ КАК ПРАВИЛЬНО ВЫВЕСТИ РЕЗУЛЬТАТЫ
+
+  //   alert("Наша история очень интересная, после прочтения нашего рассказа будьте готовы ответить на наши вопросы. В глухом лесу стояла избушка, окутанная легендами и загадками. Ее стены покрыты  вьющимися растениями, словно природа сама пыталась скрыть этот тайный уголок от посторонних глаз. Внутри избушки пахло древностью и магией, словно вся история этого места проникла в каждый уголок.");
+  //   let yes = Number(0);
+  //   alert("Вопрос 1: Чем была покрыта избушка в лесу?");
+  //   let answer_1 = confirm("Мхом");
+  //   let answer_2 = confirm("Цветами");
+  //   let answer_3 = confirm("Вьющимися растениями");
+  //   if (answer_3 == true) {
+  //       yes += 2;
+  //       } 
+  // alert("Вопрос 2: От кого была спрятана избушка?");
+  // answer_1 = confirm("Солнце");
+  // answer_2 = confirm("Дождь");
+  // answer_3 = confirm("Постороние глаза");
+  // if (answer_3 == true) {
+  //     yes += 2;
+  // }
+  // alert("Вопрос 3: Чем пахло внутри избушки?");
+  // answer_1 = confirm("Затхлостью");
+  // answer_2 = confirm("Древностью");
+  // answer_3 = confirm("Выпечкой");
+  // if (answer_2 == true) {
+  //   yes += 2;
+  // } if (yes > 0) {
+  //   alert(`Вы правильно ответили на вопросы вы получили ${yes}  баллов`);
+  //   } else {
+  //     alert(`Вы не правильно ответили на ${(yes)} вопросов `);
+  //     }
+
+
+                        // ЗАДАНИЕ 10
+                        //  НЕ ПОНЯЛ
+                          
+
+                     
